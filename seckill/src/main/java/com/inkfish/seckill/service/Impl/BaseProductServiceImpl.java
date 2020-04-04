280dd37aad34674b2b6ae61078951d026cdd4f1b
HALOXIAO <584714929@qq.com>
2020-04-04 16:50:58 +0800

    Product Service

--- seckill/src/main/java/com/inkfish/seckill/service/Impl/BaseProductServiceImpl.java
+++ seckill/src/main/java/com/inkfish/seckill/service/Impl/BaseProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.inkfish.seckill.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.inkfish.seckill.mapper.ProductMapper;
 import com.inkfish.seckill.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,18 @@ public class BaseProductServiceImpl {
 
     public boolean addProduct(Product product) {
         return productMapper.save(product);
+    }
+
+    public boolean deleteProduct(Integer id) {
+        return productMapper.remove(new QueryWrapper<Product>().eq("id", id));
+    }
+
+    public boolean updateProduct(Product product) {
+        return productMapper.update(product, new QueryWrapper<Product>().eq("id", product.getId()));
+    }
+
+    public Product getProduct(){
+        return null;
     }
 
 }
